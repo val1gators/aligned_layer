d3e1c764304aabacd15a79e2a5ba8eefdb447c8f
JuArce <52429267+JuArce@users.noreply.github.com>
2024-05-08 15:54:35 -0300

    refactor: change rpc client to grpc

--- operator/pkg/rpc_client.go
+++ operator/pkg/rpc_client.go
@@ -1,7 +1,11 @@
 package operator
 
 import (
+	"context"
+	"crypto/tls"
 	"errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"net/rpc"
 	"time"
 
@@ -11,7 +15,7 @@ import (
 
 // AggregatorRpcClient is the client to communicate with the aggregator via RPC
 type AggregatorRpcClient struct {
-	rpcClient            *rpc.Client
+	rpcClient            *grpc.ClientConn
 	aggregatorIpPortAddr string
 	logger               logging.Logger
 }
@@ -22,7 +26,7 @@ const (
 )
 
 func NewAggregatorRpcClient(aggregatorIpPortAddr string, logger logging.Logger) (*AggregatorRpcClient, error) {
-	client, err := rpc.DialHTTP("tcp", aggregatorIpPortAddr)
+	client, err := newClient(aggregatorIpPortAddr)
 	if err != nil {
 		return nil, err
 	}
@@ -34,17 +38,23 @@ func NewAggregatorRpcClient(aggregatorIpPortAddr string, logger logging.Logger) 
 	}, nil
 }
 
+func newClient(aggregatorIpPortAddr string) (*grpc.ClientConn, error) {
+	// TODO(juli): For mainnet, we should load the aggregator's certificate from a file.
+	creds := credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})
+	return grpc.NewClient(aggregatorIpPortAddr, grpc.WithTransportCredentials(creds))
+}
+
 // SendSignedTaskResponseToAggregator is the method called by operators via RPC to send
 // their signed task response.
 func (c *AggregatorRpcClient) SendSignedTaskResponseToAggregator(signedTaskResponse *types.SignedTaskResponse) {
 	var reply uint8
 	for retries := 0; retries < MaxRetries; retries++ {
-		err := c.rpcClient.Call("Aggregator.ProcessOperatorSignedTaskResponse", signedTaskResponse, &reply)
+		err := c.rpcClient.Invoke(context.Background(), "Aggregator.ProcessOperatorSignedTaskResponse", signedTaskResponse, &reply)
 		if err != nil {
 			c.logger.Error("Received error from aggregator", "err", err)
 			if errors.Is(err, rpc.ErrShutdown) {
 				c.logger.Error("Aggregator is shutdown. Reconnecting...")
-				client, err := rpc.DialHTTP("tcp", c.aggregatorIpPortAddr)
+				client, err := newClient(c.aggregatorIpPortAddr)
 				if err != nil {
 					c.logger.Error("Could not reconnect to aggregator", "err", err)
 					time.Sleep(RetryInterval)
